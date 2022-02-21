720c6e06759eca2694e888442b164e24ebcaca1b
ZGApps <appletonzak@gmail.com>
2022-02-21 10:04:03 -0600

    first commit

---
+++ src/main/java/com/zak/core/RpGenApplication.java
@@ -0,0 +1,13 @@
+package com.zak.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpGenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zak/core/RpGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zak.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
